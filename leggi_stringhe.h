9724e9b8e58123e60aeedf773434592d6f73806c
rudiganzi <rodolfoganzy@gmail.com>
2020-05-04 19:14:52 +0100

    file.h

---
+++ leggi_stringhe.h
@@ -0,0 +1,10 @@
+
+
+#ifndef LEGGI_STRINGHE_H
+#define LEGGI_STRINGHE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char** leggi_stringhe(const char* filename, size_t* size);
+#endif // !LEGGI_STRINGHE_H
